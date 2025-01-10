78974bcd2051be3cdd376ae3ef3cd5f56b368029
AP0302 <aaryanp0302@gmail.com>
2025-01-10 14:10:50 -0800

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fbla_mobile_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fbla_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fbla_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
